c4721e9546948f2d01253da8f62475c81960b2ca
Abhay <abhayrodriguezcara@gmail.com>
2023-04-11 14:24:24 -0300

    Agregue Primero.C!

---
+++ Primero.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+float: num_uno, potencia;
+main()
+{
+	printf ("Ingresa el Primer Valor");
+	Scanf ("%of,"&num_uno");
+	potencia= num_uno,*num_uno;
+	printf (El resultado es &f,"Potencia");
+}
